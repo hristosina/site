33d0dd993c240a4f4392a10a924802e060ec01b2
hristosina <56290514+hristosina@users.noreply.github.com>
2019-12-10 17:19:23 +0300

    Create site.sql

---
+++ site.sql
@@ -0,0 +1,6 @@
+ CREATE TABLE IF NOT EXISTS 'users' (
+ 'id' int(3) NOT NULL AUTO_INCREMENT ,
+ 'email' varchar(30) NOT NULL
+ 'name' varchar(20) NOT NULL,
+ 'password' varchar(30) NOT NULL,
+ 'admin' int(1) NOT NULL);
