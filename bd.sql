a24053df3f281449446bb3c31f28547056e40c8d
hristosina <56290514+hristosina@users.noreply.github.com>
2019-12-11 22:13:33 +0300

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -1,7 +1,14 @@
  CREATE DATABASE 'site';
+ 
  CREATE TABLE IF NOT EXISTS 'users' (
  'id' int(3) NOT NULL AUTO_INCREMENT ,
  'email' varchar(30) NOT NULL
  'name' varchar(20) NOT NULL,
  'password' varchar(30) NOT NULL,
  'admin' int(1) NOT NULL);
+ 
+CREATE TABLE IF NOT EXISTS 'books' (
+`id` INT(3) NOT NULL AUTO_INCREMENT , 
+`book` VARCHAR NOT NULL , `about` TEXT NOT NULL ,
+`pic` MEDIUMBLOB NOT NULL ,
+`file` MEDIUMTEXT NOT NULL);
