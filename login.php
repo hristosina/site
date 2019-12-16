f595b6d5ae92d5e9355884501bd3a69649b2f543
hristosina <56290514+hristosina@users.noreply.github.com>
2019-12-16 16:29:52 +0300

    Update login.php

--- login.php
+++ login.php
@@ -6,18 +6,17 @@
  <link rel="stylesheet" href="style.css">
 </head>
 <header>
-  <a href="" class="logo">Библиотека</a>
+  <a href="main.php" class="logo">Библиотека</a>
   <nav>
       <ul class="topmenu">
         <li><a href="main.php">Главная</a></li>
         <li><a href="catalog.php" class="submenu-link">Категория</a>
           <ul class="submenu">
-            <li><a href="">Понравилось пользователям</a></li>
-            <li><a href="">Русская литература</a></li>
-            <li><a href="">Зарубежная литература</a></li>
+            <li><a href="rus.php">Русская литература</a></li>
+            <li><a href="zar.php">Зарубежная литература</a></li>
           </ul>
         </li>
-        <li><a href="search.php">Поиск</a></li>
+        <li><a href="reg.php">Регистрация</a></li>
         <li><?php if($_COOKIE['user'] ==''): ?><a href="login.php">Войти</a><?php else: ?><a href="exit.php">Выйти</a><?php endif; ?></li>
       </ul>
     </nav>
@@ -31,8 +30,8 @@
     <h1>Войти</h1>
     <p>Пожалуйста, введите данные для входа.</p>
     <hr>
-    <label for="email"><b>Логин</b></label>
-    <input type="text" placeholder="Введите логин" name="email" required>
+    <label for="email"><b>E-mail</b></label>
+    <input type="text" placeholder="Введите email" name="email" required>
     <label for="password"><b>Пароль</b></label>
     <input type="password" placeholder="Введите пароль" name="password" required>
     <button type="submit">Войти</button> <p style="text-align: right">Ещё не <a style="color: #D5B45B" href="reg.php">зарегистрированы?</a></p>
