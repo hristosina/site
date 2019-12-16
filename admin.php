967ad3cddf8b882e409a8b8c0b817dec9b36982a
hristosina <56290514+hristosina@users.noreply.github.com>
2019-12-16 16:16:40 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -6,19 +6,25 @@
  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<form action="" method="post">
-<div>
+<div style="margin-left: auto; margin-right: auto; margin-top: 15px; width: 500px; border: 3px solid #e8e8e8; padding: 8px; background-color: white;">
+<?php
+   if($_COOKIE['user'] ==''):
+     ?>
+<p>Доступ закрыт неавторизированным пользователям.<a href="login.php" style="color: #D5B45B">Войдите как админ</a>.</p>
+<?php elseif ($_COOKIE['admin'] =='0'): ?>
+<p>Вы не админ.</p>
+<?php else: ?>
 <h1>Добро пожаловать в панель админа.</h1>
 <hr>
 <h3>
 <p>Вы можете:</p>
 <ul>
 <li><a href="add.php" style="color: #D5B45B">Добавить новую книгу</a></li>
-<li><a href="" style="color: #D5B45B">Удалить книгу</a></li>
-<li><a href="" style="color: #D5B45B">Вывести книгу в топ</a></li>
+<li><a href="delete.php" style="color: #D5B45B">Удалить книгу</a></li>
+<li><a href="main.php" style="color: #D5B45B">На главную</a></li>
 </ul>
 </h3>
+<?php endif; ?></p>
 </div>
-</form>
 </body>
 </html> 
